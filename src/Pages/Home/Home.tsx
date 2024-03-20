a45ad3420823b1ac37acff0686fa771a7de18087
Emerson Moreira <eemr3@yahoo.com.br>
2024-03-20 15:33:26 -0300

    feat: add components header, tab e footer

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,19 +1,72 @@
-import React, { useState } from 'react';
-import Sidebar from '../../Components/Siderbar/Sidebar';
-import style from './style.module.css';
-import Content from '../../Components/Content/Content';
-import TabsComponent from '../../Components/Tabs';
+import { Card } from '../../Components/Card';
+import Footer from '../../Components/Footer/Footer';
+import { Header } from '../../Components/Header/Header';
+import { Tab, Tabs } from '../../Components/Tabs';
+import { drinksData, foodData, groceryData, promotionsData } from '../../data/data';
 
 const Home = () => {
-    const [selectedOption, setSelectedOption] = useState('1');
-
-    return(
-        <div className={style.home}>
-           {/*<Sidebar setSelectedOption={setSelectedOption} /> */}
-          <TabsComponent />
-          {/*<Content selectedOption={selectedOption} />*/}
-        </div>
-    );
-}
+  return (
+    <div>
+      <Header />
+      <div className="py-16">
+        <Tabs>
+          <Tab label="PROMOÇÕES">
+            <div className="grid md:grid-cols-2 sm:grid-cols-1 lg:grid-cols-4 gap-4">
+              {promotionsData.map((promotion) => (
+                <Card
+                  key={promotion.id}
+                  image={promotion.image}
+                  name={promotion.name}
+                  description={promotion.description}
+                  price={+promotion.price}
+                />
+              ))}
+            </div>
+          </Tab>
+          <Tab label="LANCHES">
+            <div className="grid md:grid-cols-2 sm:grid-cols-1 lg:grid-cols-4 gap-4">
+              {drinksData.map((drink) => (
+                <Card
+                  key={drink.id}
+                  image={drink.image}
+                  name={drink.name}
+                  description={drink.description}
+                  price={+drink.price}
+                />
+              ))}
+            </div>
+          </Tab>
+          <Tab label="BEBIDAS">
+            <div className="grid md:grid-cols-2 sm:grid-cols-1 lg:grid-cols-4 gap-4">
+              {foodData.map((food) => (
+                <Card
+                  key={food.id}
+                  image={food.image}
+                  name={food.name}
+                  description={food.description}
+                  price={food.price}
+                />
+              ))}
+            </div>
+          </Tab>
+          <Tab label="MERCEARIA">
+            <div className="grid md:grid-cols-2 sm:grid-cols-1 lg:grid-cols-4 gap-4">
+              {groceryData.map((grocery) => (
+                <Card
+                  key={grocery.id}
+                  image={grocery.image}
+                  name={grocery.name}
+                  description={grocery.description}
+                  price={+grocery.price}
+                />
+              ))}
+            </div>
+          </Tab>
+        </Tabs>
+      </div>
+      <Footer />
+    </div>
+  );
+};
 
 export default Home;
