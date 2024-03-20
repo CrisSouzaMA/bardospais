68d3b43cce45fb7a8014cc432cca15fe05f7ab83
Emerson Moreira <eemr3@yahoo.com.br>
2024-03-20 15:32:29 -0300

    feat: cria component header

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,15 +1,26 @@
-import React from 'react';
-import styles from './style.module.css';
-import { MenuOutlined } from '@ant-design/icons';
-import { Button } from 'antd';
+import { FaCartPlus } from 'react-icons/fa';
+export const Header = () => {
+  return (
+    <>
+      <header className="sm:px-8 px-4 py-4 z-10 w-full bg-[#D9D9D9] fixed">
+        <nav className="flex justify-between items-center max-container">
+          <a href="/" className="text-3xl font-bold uppercase">
+            Bar dos Pais
+          </a>
 
-const Header = () => {
-   return(
-    <div className={styles.headerStyle}>
-      <Button type="text" className={styles.iconCss} icon={<MenuOutlined  />} />
-      <p>BAR DOS PAIS</p>
-    </div>
-   ); 
+          <button className="relative flex gap-2 text-lg leading-normal font-medium font-montserrat wide:mr-24">
+            <div className="max-lg:block cursor-pointer flex flex-col">
+              <div
+                className="rounded-full bg-red-500 text-yellow-50 text-[12px] w-5 h-5 flex 
+              justify-center items-center absolute right-[-8px] top-[-12px]"
+              >
+                12
+              </div>
+              <FaCartPlus className="text-3xl text-gray-700" />
+            </div>
+          </button>
+        </nav>
+      </header>
+    </>
+  );
 };
-
-export default Header;
