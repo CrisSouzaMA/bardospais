74b03c55bf9ea8cd59cce7207df1f2c414065b84
Emerson Moreira <eemr3@yahoo.com.br>
2024-03-20 16:48:09 -0300

    feat: adiciona "context" e funções de adicionar e remover item do carrinho

--- src/Components/Card/index.tsx
+++ src/Components/Card/index.tsx
@@ -1,13 +1,19 @@
+import { useState } from 'react';
 import { formatNumber } from '../../util/formatNumber';
+import { CardProps, DataCardProps } from '../../services/interface/card';
+import { useCart } from '../../Context/CartContext';
 
-interface CardProps {
-  image: string;
-  name: string;
-  description: string;
-  price: number;
-}
+export const Card = ({ data }: CardProps) => {
+  const { addToCart, removeFromCart } = useCart();
 
-export const Card = ({ image, name, description, price }: CardProps) => {
+  const handleAddProduct = () => {
+    addToCart(data);
+  };
+
+  const handleRemoveProduct = (id: number) => {
+    removeFromCart(id);
+  };
+
   return (
     <div
       className="mx-auto mt-11 w-80 transform overflow-hidden
@@ -16,23 +22,38 @@ export const Card = ({ image, name, description, price }: CardProps) => {
     >
       <img
         className="h-48 w-full object-cover object-center"
-        src={image}
+        src={data.image}
         alt="Product Image"
       />
       <div className="p-4">
-        <h2 className="mb-2 text-lg font-medium dark:text-white text-gray-900">{name}</h2>
-        <p className="mb-2 text-base dark:text-gray-300 text-gray-700">{description}</p>
+        <h2 className="mb-2 text-lg font-medium dark:text-white text-gray-900">
+          {data.name}
+        </h2>
+        <p className="mb-2 text-base dark:text-gray-300 text-gray-700">
+          {data.description}
+        </p>
         <div>
           <div className="">
             <p className="mr-2 text-lg font-semibold text-gray-900 dark:text-white">
-              {formatNumber(price)}
+              {formatNumber(data.price)}
             </p>
           </div>
         </div>
       </div>
       <div className="w-full bg-gray-300 text-center">
-        <button className="ml-auto text-2xl font-bold text-green-500 mr-4">+</button>0
-        <button className="text-2xl font-bold text-red-500 ml-4">-</button>
+        <button
+          className="ml-auto text-2xl font-bold text-green-500 mr-4"
+          onClick={handleAddProduct}
+        >
+          +
+        </button>
+        0
+        <button
+          className="text-2xl font-bold text-red-500 ml-4"
+          onClick={() => handleRemoveProduct(data.id)}
+        >
+          -
+        </button>
       </div>
     </div>
   );
