44a7d1e8011ada595e39c02ffa8d1cd96d74d2b0
Emerson Moreira <eemr3@yahoo.com.br>
2024-03-20 15:31:59 -0300

    feature: cria componete card

--- src/Components/Card/index.tsx
+++ src/Components/Card/index.tsx
@@ -1,41 +1,39 @@
-/* eslint-disable react-hooks/rules-of-hooks */
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import React, { useEffect } from 'react';
-import { productList } from '../listProducts';
+import { formatNumber } from '../../util/formatNumber';
 
-type Product = {
-    id: number;
-    category: string;
-    name: string;
-    description: string;
-    image: string;
-    price: string;
-    available: boolean;
-};
-
-type ProductList = {
-  [category: string]: Product[];
-};
-
-interface CardContentProps {
-    category: string;
+interface CardProps {
+  image: string;
+  name: string;
+  description: string;
+  price: number;
 }
 
-const CardContent = ({ category }: CardContentProps) => {
-
-    const products: Product[] = productList.filter((product: any) => product.category === category);
-
-    useEffect(() => {
-        console.log({ products });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
-
-
-    return (
+export const Card = ({ image, name, description, price }: CardProps) => {
+  return (
+    <div
+      className="mx-auto mt-11 w-80 transform overflow-hidden
+     rounded-lg bg-white dark:bg-slate-800 shadow-md duration-300
+      hover:scale-105 hover:shadow-lg"
+    >
+      <img
+        className="h-48 w-full object-cover object-center"
+        src={image}
+        alt="Product Image"
+      />
+      <div className="p-4">
+        <h2 className="mb-2 text-lg font-medium dark:text-white text-gray-900">{name}</h2>
+        <p className="mb-2 text-base dark:text-gray-300 text-gray-700">{description}</p>
         <div>
-       Oi
+          <div className="">
+            <p className="mr-2 text-lg font-semibold text-gray-900 dark:text-white">
+              {formatNumber(price)}
+            </p>
+          </div>
         </div>
-    );
-}
-
-export default CardContent;
+      </div>
+      <div className="w-full bg-gray-300 text-center">
+        <button className="ml-auto text-2xl font-bold text-green-500 mr-4">+</button>0
+        <button className="text-2xl font-bold text-red-500 ml-4">-</button>
+      </div>
+    </div>
+  );
+};
