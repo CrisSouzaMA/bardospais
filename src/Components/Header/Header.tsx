74cd3997d6908fec1d6f42d446ec6ae56130a08a
Emerson Moreira <eemr3@yahoo.com.br>
2024-03-20 16:48:32 -0300

    feat: adiciona context e array de produtos "cart"

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,5 +1,8 @@
 import { FaCartPlus } from 'react-icons/fa';
+import { useCart } from '../../Context/CartContext';
+
 export const Header = () => {
+  const { cart } = useCart();
   return (
     <>
       <header className="sm:px-8 px-4 py-4 z-10 w-full bg-[#D9D9D9] fixed">
@@ -14,7 +17,7 @@ export const Header = () => {
                 className="rounded-full bg-red-500 text-yellow-50 text-[12px] w-5 h-5 flex 
               justify-center items-center absolute right-[-8px] top-[-12px]"
               >
-                12
+                {cart.reduce((acc, item) => acc + (item.quantity || 0), 0)}
               </div>
               <FaCartPlus className="text-3xl text-gray-700" />
             </div>
